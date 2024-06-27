a4cb5f7b8f9107ee8ea15af5a5dd6c1b97672892
Serhii Vasyliev <vv44980@gmail.com>
2024-06-27 09:03:24 -0600

    tests are done

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,8 @@ const { defineConfig } = require('cypress');
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://www.globalsqa.com/angularJs-protractor/BankingProject/',
+    viewportHeight: 1300,
+    viewportWidth: 1200,
     setupNodeEvents(on, config) {
     }
   }
